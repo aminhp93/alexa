dd25686b1d652523358f2c48603a1168a7dea962
aminhp93 <minhpn.org.ec@gmail.com>
2016-09-28 12:42:36 -0700

    stable version for lambda v1

---
+++ alexa/AWS.py
@@ -0,0 +1,57 @@
+#----------------------------------------- Intent Schema Below:------------------------------
+
+{
+  "intents": [
+    {
+      "intent": "DojoInfoIntent"
+    },
+    {
+      "intent": "DojoStaffIntent"
+    },  
+    {
+      "intent": "AMAZON.HelpIntent"
+    },
+    {
+      "intent": "AMAZON.StopIntent"
+    },
+    {
+      "intent": "DojoStackIntent",
+      "slots" : [
+        {
+          "name": "City",
+          "type": "CITIES"
+        }
+      ]
+    },
+    {
+      "intent" : "TextBrendenIntent"
+    },
+    {
+      "intent" : "GetTouchFaceIntent"
+    },
+    {
+      "intent" : "DojoBrendenIntent"
+    },
+    {
+      "intent" : "AskBrendan"
+    }
+
+  ]
+}
+
+
+#----------------------------------------- Utterances Below:------------------------------
+
+
+DojoInfoIntent what is the coding dojo
+DojoInfoIntent tell me about the coding dojo
+DojoStaffIntent who are the instructors 
+DojoStaffIntent how many instructors work at coding dojo
+DojoStackIntent which stacks does {City} teach
+TextBrendenIntent Text Brendan
+GetTouchFaceIntent Tell what does Brenden say
+DojoBrendenIntent who is brenden
+AskBrendan what is touchface
+
+
+
